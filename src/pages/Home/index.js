b95cf74a50699815f087de3008324ac66784b99e
FelipeDiasD <prado.dias@hotmail.com>
2022-05-30 17:08:24 -0300

    First commit - initial code

---
+++ src/pages/Home/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+export default function Home() {
+  return (
+    <View>
+      <Text>This is Home!</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  }
+})
